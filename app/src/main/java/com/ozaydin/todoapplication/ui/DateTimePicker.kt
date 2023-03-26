c3d7fe6339d1c4d76b9bd7f68f6bfcfe16a1f8c9
ahmetozydn <ahmetozaydn944@gmail.com>
2023-03-26 23:37:25 +0300

    added date-time library

---
+++ app/src/main/java/com/ozaydin/todoapplication/ui/DateTimePicker.kt
@@ -0,0 +1,43 @@
+package com.ozaydin.todoapplication.viewmodel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ozaydin.todoapplication.theme.ToDoApplicationTheme
+
+class DateTimePicker : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting2("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview2() {
+    ToDoApplicationTheme {
+        Greeting2("Android")
+    }
+}
