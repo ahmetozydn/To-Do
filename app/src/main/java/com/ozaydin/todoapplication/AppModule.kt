71a1e55d25db09f134e302fb066ce52120f0da77
ahmetozydn <ahmetozaydn944@gmail.com>
2023-03-30 15:52:01 +0300

    added: Dagger-Hilt

--- app/src/main/java/com/ozaydin/todoapplication/AppModule.kt
+++ app/src/main/java/com/ozaydin/todoapplication/AppModule.kt
@@ -1,4 +1,4 @@
 package com.ozaydin.todoapplication
 
-class ToDoApplication {
+class AppModule {
 }
---
+++ app/src/main/java/com/ozaydin/todoapplication/BaseApplication.kt
@@ -0,0 +1,7 @@
+package com.ozaydin.todoapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ToDoApplication : Application() {}
