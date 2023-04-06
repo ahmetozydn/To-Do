7fdd94fb067c19eb8e79f2b79b18a79486d4b73f
ahmetozydn <ahmetozaydn944@gmail.com>
2023-04-06 14:32:07 +0300

    added: search feature

--- app/src/main/java/com/ozaydin/todoapplication/AppModule.kt
+++
@@ -1,4 +0,0 @@
-package com.ozaydin.todoapplication
-
-class AppModule {
-}
---
+++ app/src/main/java/com/ozaydin/todoapplication/di/AppModule.kt
@@ -0,0 +1,43 @@
+package com.ozaydin.todoapplication
+
+import android.content.Context
+import androidx.room.Room
+import com.ozaydin.todoapplication.data.Task
+import com.ozaydin.todoapplication.data.TaskDao
+import com.ozaydin.todoapplication.data.TaskDatabase
+import com.ozaydin.todoapplication.repository.TaskRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    fun provideChannelDao(db: TaskDatabase): TaskDao {
+        return db.taskDao()
+    }
+/*    @ApplicationContext
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(context = context,
+        TaskDatabase::class.java,
+        "task_database").build()*/
+    @Provides
+    @Singleton // ensure only one instance would be created
+    fun provideAppDatabase(@ApplicationContext appContext: Context): TaskDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            TaskDatabase::class.java,
+            "task_database"
+        ).build()
+    }
+    @Provides
+    fun provideRepo(db:TaskDatabase) =
+        TaskRepository(db);
+}
+
